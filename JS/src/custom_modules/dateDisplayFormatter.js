1c54e009136cb1b55a2abb53ac0ea8fbef6b7ce4
Vladislav G <ravengesport@gmail.com>
2019-09-03 10:43:34 +0300

    added more templates

--- JS/src/custom_modules/dateDisplayFormatter.js
+++ JS/src/custom_modules/dateDisplayFormatter.js
@@ -68,11 +68,48 @@ export class DateDisplayFormatter {
             case "YYYY-MM-DD": {
                 return `${this.date.year}-${this.date.month}-${this.date.day}`;
             }
+            case "YYYYDDMM": {
+                return `${this.date.year} ${this.date.day} ${this._getMonth()}`;
+            }
+            case "YYYY-DD-MM": {
+                return `${this.date.year}-${this.date.day}-${this.date.month}`;
+            }
             case "MMDDYYYY": {
                 return `${this._getMonth()} ${this.date.day} ${this.date.year}`;
             }
             case "MM-DD-YYYY": {
                 return `${this.date.month}-${this.date.day}-${this.date.year}`;
+            }
+            case "DDMMYYYY": {
+                return `${this.date.day} ${this._getMonth()} ${this.date.year}`;
+            }
+            case "DD-MM-YYYY": {
+                return `${this.date.day}-${this.date.month}-${this.date.year}`;
+            }
+
+            case "YYMMDD": {
+                return `${this.date.year.substring(this.date.year.length-2)} ${this._getMonth()} ${this.date.day}`;
+            }
+            case "YY-MM-DD": {
+                return `${this.date.year.substring(this.date.year.length-2)}-${this.date.month}-${this.date.day}`;
+            }
+            case "YYDDMM": {
+                return `${this.date.year.substring(this.date.year.length-2)} ${this.date.day} ${this._getMonth()}`;
+            }
+            case "YY-DD-MM": {
+                return `${this.date.year.substring(this.date.year.length-2)}-${this.date.day}-${this.date.month}`;
+            }
+            case "MMDDYY": {
+                return `${this._getMonth()} ${this.date.day} ${this.date.year.substring(this.date.year.length-2)}`;
+            }
+            case "MM-DD-YY": {
+                return `${this.date.month}-${this.date.day}-${this.date.year.substring(this.date.year.length-2)}`;
+            }
+            case "DDMMYY": {
+                return `${this.date.day} ${this._getMonth()} ${this.date.year.substring(this.date.year.length-2)}`;
+            }
+            case "DD-MM-YY": {
+                return `${this.date.day}-${this.date.month}-${this.date.year.substring(this.date.year.length-2)}`;
             }
             default: {
                 return "That format invalid or not supported yet.";
@@ -134,6 +171,30 @@ export class DateDisplayFormatter {
                 this.date.display = `${this.date.day}-${this.date.month}-${this.date.year}`
                 return this.date.display;
             }
+            case "YYYYDDMM": {
+                const reg = /([12]\d{3})(0[1-9]|[12]\d|3[01])(0[1-9]|1[0-2])/;
+                const formatted = this._input.match(reg);
+                if(!formatted) return null;
+                this.date = {
+                    year: formatted[1],
+                    month: formatted[2],
+                    day: formatted[3]
+                }
+                this.date.display = `${this.date.day} ${this._getMonth()}-${this.date.year}`
+                return this.date.display;
+            }
+            case "YYYY-DD-MM": {
+                const reg = /([12]\d{3})-(0[1-9]|[12]\d|3[01])-(0[1-9]|1[0-2])/;
+                const formatted = this._input.match(reg);
+                if(!formatted) return null;
+                this.date = {
+                    year: formatted[1],
+                    month: formatted[2],
+                    day: formatted[3]
+                }
+                this.date.display = `${this.date.day}-${this.date.month}-${this.date.year}`
+                return this.date.display;
+            }
             case "MMDDYYYY": {
                 const reg = /(0[1-9]|1[0-2])(0[1-9]|[12]\d|3[01])([12]\d{3})/;
                 const formatted = this._input.match(reg);
@@ -154,6 +215,127 @@ export class DateDisplayFormatter {
                     year: formatted[3],
                     month: formatted[1],
                     day: formatted[2]
+                };
+                this.date.display = `${this.date.day}-${this.date.month}-${this.date.year}`
+                return this.date.display;
+            }
+            case "DD-MM-YYYY": {
+                const reg = /(0[1-9]|[12]\d|3[01])(0[1-9]|1[0-2])([12]\d{3})/;
+                const formatted = this._input.match(reg);
+                if(!formatted) return null;
+                this.date = {
+                    year: formatted[3],
+                    month: formatted[2],
+                    day: formatted[1]
+                };
+                this.date.display = `${this.date.day}-${this._getMonth()}-${this.date.year}`
+                return this.date.display;
+            }
+            case "DDMMYYYY": {
+                const reg = /(0[1-9]|[12]\d|3[01])(0[1-9]|1[0-2])([12]\d{3})/;
+                const formatted = this._input.match(reg);
+                if(!formatted) return null;
+                this.date = {
+                    year: formatted[3],
+                    month: formatted[2],
+                    day: formatted[1]
+                };
+                this.date.display = `${this.date.day}-${this.date.month}-${this.date.year}`
+                return this.date.display;
+            }
+            // ------------ YY 
+            case "YYMMDD": {
+                const reg = /(0[1-9]|[1-9][0-9])(0[1-9]|1[0-2])(0[1-9]|[12]\d|3[01])/
+                const formatted = this._input.match(reg);
+                if(!formatted) return null;
+                this.date = {
+                    year: formatted[1],
+                    month: formatted[2],
+                    day: formatted[3]
+                }
+                this.date.display = `${this.date.day} ${this._getMonth()} ${this.date.year}`
+                return this.date.display;
+            }
+            case "YY-MM-DD": {
+                const reg = /(0[1-9]|[1-9][0-9])-(0[1-9]|1[0-2])-(0[1-9]|[12]\d|3[01])/;
+                const formatted = this._input.match(reg);
+                if(!formatted) return null;
+                this.date = {
+                    year: formatted[1],
+                    month: formatted[2],
+                    day: formatted[3]
+                }
+                this.date.display = `${this.date.day}-${this.date.month}-${this.date.year}`
+                return this.date.display;
+            }
+            case "YYDDMM": {
+                const reg = /(0[1-9]|[1-9][0-9])(0[1-9]|[12]\d|3[01])(0[1-9]|1[0-2])/;
+                const formatted = this._input.match(reg);
+                if(!formatted) return null;
+                this.date = {
+                    year: formatted[1],
+                    month: formatted[2],
+                    day: formatted[3]
+                }
+                this.date.display = `${this.date.day} ${this._getMonth()}-${this.date.year}`
+                return this.date.display;
+            }
+            case "YY-DD-MM": {
+                const reg = /(0[1-9]|[1-9][0-9])-(0[1-9]|[12]\d|3[01])-(0[1-9]|1[0-2])/;
+                const formatted = this._input.match(reg);
+                if(!formatted) return null;
+                this.date = {
+                    year: formatted[1],
+                    month: formatted[2],
+                    day: formatted[3]
+                }
+                this.date.display = `${this.date.day}-${this.date.month}-${this.date.year}`
+                return this.date.display;
+            }
+            case "MMDDYY": {
+                const reg = /(0[1-9]|1[0-2])(0[1-9]|[12]\d|3[01])(0[1-9]|[1-9][0-9])/;
+                const formatted = this._input.match(reg);
+                if(!formatted) return null;
+                this.date = {
+                    year: formatted[3],
+                    month: formatted[1],
+                    day: formatted[2]
+                };
+                this.date.display = `${this.date.day} ${this._getMonth()} ${this.date.year}`
+                return this.date.display;
+            }
+            case "MM-DD-YY": {
+                const reg = /(0[1-9]|1[0-2])(0[1-9]|[12]\d|3[01])(0[1-9]|[1-9][0-9])/;
+                const formatted = this._input.match(reg);
+                if(!formatted) return null;
+                this.date = {
+                    year: formatted[3],
+                    month: formatted[1],
+                    day: formatted[2]
+                };
+                this.date.display = `${this.date.day}-${this.date.month}-${this.date.year}`
+                return this.date.display;
+            }
+            case "DD-MM-YY": {
+                const reg = /(0[1-9]|[12]\d|3[01])(0[1-9]|1[0-2])(0[1-9]|[1-9][0-9])/;
+                const formatted = this._input.match(reg);
+                if(!formatted) return null;
+                this.date = {
+                    year: formatted[3],
+                    month: formatted[2],
+                    day: formatted[1]
+                };
+                this.date.display = `${this.date.day}-${this._getMonth()}-${this.date.year}`
+                return this.date.display;
+            }
+            case "DDMMYY": {
+                const reg = /(0[1-9]|[12]\d|3[01])(0[1-9]|1[0-2])(0[1-9]|[1-9][0-9])/;
+                const formatted = this._input.match(reg);
+                if(!formatted) return null;
+                this.date = {
+                    year: formatted[3],
+                    month: formatted[2],
+                    day: formatted[1]
                 };
                 this.date.display = `${this.date.day}-${this.date.month}-${this.date.year}`
                 return this.date.display;
