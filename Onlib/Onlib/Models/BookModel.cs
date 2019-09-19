6a672c272da6b45367ecb2c68a1bca0b40bbd2b5
Vladislav G <ravengesport@gmail.com>
2019-09-19 13:00:41 +0300

    first dbContext

--- Onlib/Onlib/Models/BookModel.cs
+++ Onlib/Onlib/Models/BookModel.cs
@@ -1,5 +1,18 @@
-﻿namespace Onlib.Models
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Onlib.Models
 {
+    class BookContext : DbContext
+    {
+        public DbSet<BookModel> Books { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<BookModel>().Property(x => x.Id).IsRequired();
+            modelBuilder.Entity<BookModel>().Property(x => x.Title).IsRequired();
+        }
+    }
+
     public class BookModel
     {
         public int Id { get; set; }
