5f5209b0836cc80ece813448b00669825f541e16
Vladislav G <ravengesport@gmail.com>
2019-10-01 13:43:49 +0300

    finally works

--- Onlib/Onlib/ClientApp/src/middleware/signalRMiddleware.js
+++ Onlib/Onlib/ClientApp/src/middleware/signalRMiddleware.js
@@ -5,6 +5,7 @@ import {
     LogLevel
 } from '@aspnet/signalr';
 import * as CommentActions from '../comment/actions';
+import * as BookActions from '../book/actions';
 import { bindActionCreators } from 'redux';
 
 
@@ -30,19 +31,24 @@ const connection = new HubConnectionBuilder()
 const startSignalRConnection = connection => connection.start()
     .then(() => console.info('SignalR Connected'))
     .catch(err => console.error('SignalR Connection Error: ', err));
-    
+
 startSignalRConnection(connection);
-  
+
 const signalRMiddleware = store => next => async (action) => {
 
     if (action.type === CommentActions.ADD_COMMENT_SUCCESS) {
         let bookId = store.getState().book.currentBook.id
         connection.invoke("UpdateComments", bookId);
+    }
+    if (action.type === BookActions.BOOK_SUCCESS) {
         connection.on("CommentsChanged", (id) => {
             console.log(id);
             let load = bindActionCreators(CommentActions.commentOperations, store.dispatch).loadComments
             load(id);
         })
+    }
+    if (action.type === BookActions.BOOKS_REQUEST) {
+        connection.off("CommentsChanged");
     }
 
     return next(action);
